47c4e262f484ddf4937d09f939e74ec1e3248493
Angelic <0andydean19@gmail.com>
2024-12-07 20:09:21 -0500

    Initial commit

---
+++ src/main/java/net/angelic/weaponsexpanded/WeaponsExpanded.java
@@ -0,0 +1,16 @@
+package net.angelic.weaponsexpanded;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WeaponsExpanded implements ModInitializer {
+	public static final String MOD_ID = "weaponsexpanded";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/angelic/weaponsexpanded/WeaponsExpandedClient.java
@@ -0,0 +1,10 @@
+package net.angelic.weaponsexpanded;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeaponsExpandedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
