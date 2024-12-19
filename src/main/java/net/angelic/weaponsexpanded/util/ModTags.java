e09a69bf97d33f6f492a6aa1c3ef452a2afd09d2
Angelic <0andydean19@gmail.com>
2024-12-19 00:03:01 -0500

    changed frostbite.png

--- src/main/java/net/angelic/weaponsexpanded/util/ModTags.java
+++ src/main/java/net/angelic/weaponsexpanded/util/ModTags.java
@@ -9,14 +9,12 @@ import net.minecraft.util.Identifier;
 
 public class ModTags {
     public static class items {
-        public static final TagKey<Item> TWO_HANDED = createTag("two_handed");
 
         private static TagKey<Item> createTag(String name) {
             return TagKey.of(RegistryKeys.ITEM, Identifier.of(WeaponsExpanded.MOD_ID, name));
         }
     }
     public static class enchantments {
-        public static final TagKey<Enchantment> POST_HIT = createTag("post_hit");
 
         private static TagKey<Enchantment> createTag(String name) {
             return TagKey.of(RegistryKeys.ENCHANTMENT, Identifier.of(WeaponsExpanded.MOD_ID, name));
